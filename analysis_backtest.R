07ff09740efff443bd062a29d39393c404b8eb11
SalisEduardo <amorimsalis.edu@usp.br>
2023-02-08 01:38:54 -0300

    New time series plots

--- analysis_backtest.R
+++ analysis_backtest.R
@@ -30,7 +30,8 @@ policy_keyval <- c("maxSR"="MSR","EW"="EWP","PropInef"="INBP","InvInef"="EPB")
 
 
 STRATS_DEOPTM_4 <- readRDS(file = "STRATS_DEOPTM_4.RDS") 
-KPIS_backtest_4 <- readRDS(file = "KPIS_backtest_4.RDS")  %>% 
+KPIS_backtest_4 <- readRDS(file = "KPIS_backtest_4.RDS")  
+KPIS_backtest_4 <- KPIS_backtest_4 %>% 
   dplyr::mutate(`Annualized Std Dev` = `Annualized Std Dev` * sqrt(360/252)) %>% # Annualize by 360
   dplyr::mutate(Efficiency_group = ifelse(KPIS_backtest_4$Efficiency_group=="most","more",KPIS_backtest_4$Efficiency_group)) %>% 
   dplyr::mutate(Policy = ifelse(KPIS_backtest_4$Policy %in% names(policy_keyval), policy_keyval[KPIS_backtest_4$Policy], KPIS_backtest_4$Policy))
@@ -38,7 +39,8 @@ KPIS_backtest_4 <- readRDS(file = "KPIS_backtest_4.RDS")  %>%
 
 
 STRATS_DEOPTM_index <- readRDS(file = "STRATS_DEOPTM_index.RDS") 
-KPIS_backtest_index <- readRDS(file = "KPIS_backtest_index.RDS") %>% 
+KPIS_backtest_index <- readRDS(file = "KPIS_backtest_index.RDS") 
+KPIS_backtest_index <- KPIS_backtest_index %>% 
   dplyr::rename("Efficiency_group"=Efficency_group) %>% #spelling mistake
   dplyr::mutate(`Annualized Std Dev` = `Annualized Std Dev` * sqrt(360/252)) %>% # Annualize by 360
   dplyr::mutate(Policy = ifelse(KPIS_backtest_index$Policy %in% names(policy_keyval), policy_keyval[KPIS_backtest_index$Policy], KPIS_backtest_index$Policy))
@@ -91,10 +93,10 @@ scatterplot_risk_return <- function(kpi,year,n_assets = NULL,folder_path="Result
                         color=Efficiency_group,
                         label=Policy),
                     hjust=0, vjust=0) + 
-    xlab(TeX("$\\sigma^A_p$")) + 
-    ylab(TeX("$R^A_p$")) + 
+    xlab(latex2exp::TeX("$\\sigma^A_p$")) + 
+    ylab(latex2exp::TeX("$r^A_p$")) + 
   
-    labs(title = "",color="Efficiency Level",shape="Efficiency Level",size=TeX("$SR_p$")) + 
+    labs(title = "",color="Efficiency Level",shape="Efficiency Level",size=latex2exp::TeX("$SR_p$")) + 
     theme_bw()
   ggsave(file_plt,plt)
   plt
@@ -106,34 +108,41 @@ scatterplot_risk_return(KPIS_backtest_4,year = '2020')
 scatterplot_risk_return(KPIS_backtest_4,year = '2021')
 scatterplot_risk_return(KPIS_backtest_4,year = '2022')
 
-scatterplot_risk_return(ALL_KPI,year = '2020',n_assets = "All")
-scatterplot_risk_return(ALL_KPI,year = '2021',n_assets = "All")
-scatterplot_risk_return(ALL_KPI,year = '2022',n_assets = "All")
-
 scatterplot_risk_return(KPIS_backtest_index,year = '2020')
 scatterplot_risk_return(KPIS_backtest_index,year = '2021')
 scatterplot_risk_return(KPIS_backtest_index,year = '2022')
 
-
-
-
+scatterplot_risk_return(ALL_KPI,year = '2020',n_assets = "All")
+scatterplot_risk_return(ALL_KPI,year = '2021',n_assets = "All")
+scatterplot_risk_return(ALL_KPI,year = '2022',n_assets = "All")
 
 
 # Performance Time Series
 dailyrets_4cryptos2020 <- gatther_Rets(STRATS_DEOPTM_4,name_pattern = '2020')
-cumrets2020 <- cumrets_to_longer(dailyrets_4cryptos2020,create_group = FALSE)
-cumrets2020[c("Policy","Efficiency_group","N_assets","Test_period")]<- str_split_fixed(cumrets2020$Strat, '_', 4)
+cumrets2020_4cryptos <- cumrets_to_longer(dailyrets_4cryptos2020,create_group = FALSE)
+cumrets2020_4cryptos[c("Policy","Efficiency_group","N_assets","Test_period")]<- str_split_fixed(cumrets2020_4cryptos$Strat, '_', 4)
 
 dailyrets_4cryptos2021 <- gatther_Rets(STRATS_DEOPTM_4,name_pattern = '2021')
-cumrets2021 <- cumrets_to_longer(dailyrets_4cryptos2021,create_group = FALSE)
-cumrets2021[c("Policy","Efficiency_group","N_assets","Test_period")]<- str_split_fixed(cumrets2021$Strat, '_', 4)
+cumrets2021_4cryptos <- cumrets_to_longer(dailyrets_4cryptos2021,create_group = FALSE)
+cumrets2021_4cryptos[c("Policy","Efficiency_group","N_assets","Test_period")]<- str_split_fixed(cumrets2021_4cryptos$Strat, '_', 4)
 
 dailyrets_4cryptos2022 <- gatther_Rets(STRATS_DEOPTM_4,name_pattern = '2022')
-cumrets2022 <- cumrets_to_longer(dailyrets_4cryptos2022,create_group = FALSE)
-cumrets2022[c("Policy","Efficiency_group","N_assets","Test_period")]<- str_split_fixed(cumrets2022$Strat, '_', 4)
+cumrets2022_4cryptos <- cumrets_to_longer(dailyrets_4cryptos2022,create_group = FALSE)
+cumrets2022_4cryptos[c("Policy","Efficiency_group","N_assets","Test_period")]<- str_split_fixed(cumrets2022_4cryptos$Strat, '_', 4)
 
 
 
+dailyrets_index2020 <- gatther_Rets(STRATS_DEOPTM_index,name_pattern = '2020')
+cumrets2020_index <- cumrets_to_longer(dailyrets_index2020,create_group = FALSE)
+cumrets2020_index[c("Policy","Efficiency_group","N_assets","Test_period")]<- str_split_fixed(cumrets2020_index$Strat, '_', 4)
+
+dailyrets_index2021 <- gatther_Rets(STRATS_DEOPTM_index,name_pattern = '2021')
+cumrets2021_index <- cumrets_to_longer(dailyrets_index2021,create_group = FALSE)
+cumrets2021_index[c("Policy","Efficiency_group","N_assets","Test_period")]<- str_split_fixed(cumrets2021_index$Strat, '_', 4)
+
+dailyrets_index2022 <- gatther_Rets(STRATS_DEOPTM_index,name_pattern = '2022')
+cumrets2022_index <- cumrets_to_longer(dailyrets_index2022,create_group = FALSE)
+cumrets2022_index[c("Policy","Efficiency_group","N_assets","Test_period")]<- str_split_fixed(cumrets2022_index$Strat, '_', 4)
 
 
 consolidate_daily <- function(list_dailyrets){
@@ -148,10 +157,19 @@ consolidate_daily <- function(list_dailyrets){
 }
 
 all_dailyrets_4cryptos <- consolidate_daily(list(dailyrets_4cryptos2020,dailyrets_4cryptos2021,dailyrets_4cryptos2022))
+all_cumrets_4cryptos <- cumrets_to_longer(all_dailyrets_4cryptos,create_group = FALSE)
+all_cumrets_4cryptos[c("Policy","Efficiency_group","N_assets","Test_period")]<- str_split_fixed(all_cumrets_4cryptos$Strat, '_', 4)
 
-all_cumrets <- cumrets_to_longer(all_dailyrets_4cryptos,create_group = FALSE)
-all_cumrets[c("Policy","Efficiency_group","N_assets","Test_period")]<- str_split_fixed(all_cumrets$Strat, '_', 4)
+all_dailyrets_index <- consolidate_daily(list(dailyrets_index2020,dailyrets_index2021,dailyrets_index2022))
+all_cumrets_index <- cumrets_to_longer(all_dailyrets_index,create_group = FALSE)
+all_cumrets_index[c("Policy","Efficiency_group","N_assets","Test_period")]<- str_split_fixed(all_cumrets_index$Strat, '_', 4)
 
+
+ALL_cumrets_2020 <- rbind(cumrets2020_index,cumrets2020_4cryptos) %>%  arrange(Strat,Date)
+ALL_cumrets_2021 <- rbind(cumrets2021_index,cumrets2021_4cryptos) %>%  arrange(Strat,Date)
+ALL_cumrets_2022 <- rbind(cumrets2022_index,cumrets2022_4cryptos) %>%  arrange(Strat,Date)
+
+ALL_periods_strategies_cumrets <- rbind(ALL_cumrets_2020,ALL_cumrets_2021,ALL_cumrets_2022)
 
 
 plot_all_ts <- function(df_cumrets){
@@ -203,9 +221,47 @@ plot_all_ts <- function(df_cumrets){
 }
 
 
-plot_all_ts(all_cumrets)
-plot_all_ts(cumrets2020)
-plot_all_ts(cumrets2021)
-plot_all_ts(cumrets2022)
+plot_all_ts_v2 <- function(df_cumrets,folder_path="Results/Plots_Backtest/"){
+  #year_plt <- unique(df_cumrets$Test_period)[1] # assuming just one period
+  portfolios_groups <- unique(df_cumrets$Policy)
+  plt_list <- list()
+  i <- 1
+  for(p in portfolios_groups){
+  
+    portfolios_plt_list <- list()
+    k <- 1
+    
+    for (y in c("2020","2021","2022")) {
+      plt <- df_cumrets %>%  
+        filter(Policy == p,Test_period == y) %>% 
+        ggplot(aes(x=Date,y=return)) +
+        geom_line(aes(color = Efficiency_group,linetype = Efficiency_group), size = 1,alpha=0.68) +
+        ylab(latex2exp::TeX("$r^C_p$"))+
+        labs(x="",color='Efficiency Level',linetype = 'Efficiency Level',title = y)+
+        theme_classic()
+      
+      portfolios_plt_list[[k]] <- plt
+      k <- k + 1 
+      
+    }
+    portfolios_plt <- ggpubr::ggarrange(plotlist=portfolios_plt_list,ncol=1,nrow = 3,common.legend = TRUE) 
+    file_plt <-paste(folder_path,p,".png",sep='')
+    ggsave(file_plt,portfolios_plt)
+    print(portfolios_plt)
+    plt_list[[i]] <- portfolios_plt
+    i <- i + 1 
+  }
+  
+  return(plt_list)
+  
+}
+
+
+plot_all_ts_v2(ALL_periods_strategies_cumrets)
+
+# plot_all_ts(all_cumrets_4cryptos)
+# plot_all_ts(cumrets2020_4cryptos)
+# plot_all_ts(cumrets2021_4cryptos)
+# plot_all_ts(cumrets2022_4cryptos)
 
 
